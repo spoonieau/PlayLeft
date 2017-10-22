571923dc9018e3e577018f7657af082d56655d22
Spoonieau <spoonie.woonie@gmail.com>
2017-10-23 05:10:00 +0800

    1.3 Update

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,18 +1,11 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PlayLeft")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PlayLeft")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -24,6 +17,11 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.3.0.0")]
+[assembly: AssemblyFileVersion("1.3.0.0")]
 [assembly: ComVisible(false)]
+[assembly: AssemblyTitleAttribute("PlayLeft")]
+[assembly: AssemblyDescriptionAttribute("A small utility to display the battery status from you wireless XBOX controller.")]
+[assembly: AssemblyCompanyAttribute("System86")]
+[assembly: NeutralResourcesLanguageAttribute("en-US")]
+
